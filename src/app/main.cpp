2ba0165438c60af7e6e950544ba98dc3260a91a2
Max Howell <max.howell@methylblue.com>
2006-08-19 13:39:01 +0000

    Oops, I do still have a handbook. Credit!
    svn path=/trunk/extragear/utils/filelight/; revision=574585

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -19,7 +19,7 @@ static const KCmdLineOptions options[] =
 static KAboutData about(
         APP_NAME, I18N_NOOP( APP_PRETTYNAME ), APP_VERSION,
         I18N_NOOP("Graphical disk-usage information"), KAboutData::License_GPL_V2,
-        I18N_NOOP("(C) 2006 Max Howell"), 0,
+        I18N_NOOP("(C )2006 Max Howell"), 0,
         "http://www.methylblue.com/filelight/", "filelight@methylblue.com" );
 
 
@@ -28,6 +28,7 @@ int main( int argc, char *argv[] )
     using Filelight::MainWindow;
 
     about.addAuthor( "Max Howell", I18N_NOOP("Author, maintainer"), "max.howell@methylblue.com", "http://www.methylblue.com/" );
+    about.addAuthor( "Mike Diehl", I18N_NOOP("Documentation"), 0, 0 );
     about.addCredit( "Steffen Gerlach", I18N_NOOP("Inspiration"), 0, "http://www.steffengerlach.de/" );
     about.addCredit( "André Somers",   I18N_NOOP("Internationalization") );
     about.addCredit( "Stephanie James", I18N_NOOP("Testing") );
