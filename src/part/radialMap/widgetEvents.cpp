5eeb57364929067d740393c617f9a1c0655432c8
Martin Tobias Holmedahl Sandsmark <sandsmark@samfundet.no>
2010-09-29 22:54:43 +0000

    add filelight to the kdeutils cmakelists.txt, remember that we don't have implicit casting from const chars
    svn path=/trunk/KDE/kdeutils/filelight/; revision=1181068

--- src/part/radialMap/widgetEvents.cpp
+++ src/part/radialMap/widgetEvents.cpp
@@ -70,7 +70,7 @@ void RadialMap::Widget::paintEvent(QPaintEvent*)
         paint.drawPixmap(m_offset, m_map.pixmap());
     else
     {
-        paint.drawText(rect(), 0, i18n("We messed up, the user needs to initiate a rescan.", "Internal representation is invalid,\nplease rescan."));
+        paint.drawText(rect(), 0, i18n("We messed up, the user needs to initiate a rescan.", QLatin1String("Internal representation is invalid,\nplease rescan.")));
         return;
     }
 
