745932d29035e926f7f438c6217ed928b1d0346c
Christoph Cullmann <cullmann@kde.org>
2019-09-28 18:22:00 +0200

    improve hi-dpi support tested, seems to work properly

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,12 @@
 
 int main(int argc, char *argv[])
 {
+    /**
+     * enable high dpi support
+     */
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
+
     QApplication app(argc, argv);
 
     KLocalizedString::setApplicationDomain("filelight");
