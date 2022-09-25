be7e3ffd5bb11b1056ef41b1d8e870570d4e97d2
Laurent Montel <montel@kde.org>
2022-09-25 18:18:10 +0200

    Make compile on windows

--- src/windowsWalker.cpp
+++ src/windowsWalker.cpp
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2022 Harald Sitter <sitter@kde.org>
 
 #include "windowsWalker.h"
+#include <QScopeGuard>
 
 WindowsWalker::WindowsWalker(const QByteArray &path)
     : m_path(path)
